3d6bb872febe8a5e546bc9e362204c3f2aecc1d8
Pedro <Pedro@sg.srv>
2021-02-24 22:16:24 -0300

    puxar em casa

--- src/components/ChallengeBox.tsx
+++ src/components/ChallengeBox.tsx
@@ -4,14 +4,38 @@ import styles from '../styles/components/ChallengesBox.module.css';
 
 
 export function ChallengesBox(){
+    const hasActivateChallenger = true;
     return(
         <div className={styles.ChallengeBoxContainder}>
+            {hasActivateChallenger ? (
+                <div className={styles.challengeActivate}>
+                    <header>Ganhe 400xp</header>
+
+                    <main>
+                        <img src="icons\body.svg"/>
+                        <strong>Novo desafio</strong>
+                        <p>Descanse os olhos por alguns minutos!</p>
+                    </main>
+
+                    <footer>
+                         <button 
+                         type="button"
+                         className={styles.buttonFailChallenge}
+                         >Falhei</button>
+                         <button 
+                         type="button"
+                         className={styles.buttonSucessChallenge}
+                         >Completei</button>
+                    </footer>
+                </div>
+            ) : (
             <div className={styles.ChallengeNotActive}>
-                <strong>Finalize a count to release a challenge</strong>
+                    <strong>Finalize a count to release a challenge</strong>
                 <p>
                     <img src="icons/level-up.svg" alt=""/>
+                    Avançe de Level Completando o desafio!
                 </p>
-            </div>
+            </div>)}
         </div>
     )
 }
